b549732497f36eb9cb427dfd1e7f4de3bd70aa4e
Dean <thdtmdwo789@naver.com>
2022-10-13 18:11:18 +0900

    fix error

--- contracts/BWLBadgeFactory.sol
+++ contracts/BWLBadgeFactory.sol
@@ -13,7 +13,8 @@ $$$$$$$/  $$/  $$$$$$/   $$$$$$$/ $$/   $$/       $$/      $$/  $$$$$$$/     $/ 
 */
 
 pragma solidity >=0.7.0 <=0.9.0;
-import "./ERC1155/ERC1155.sol";
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BWLBadgeFactory is ERC1155, Ownable {
@@ -76,41 +77,6 @@ contract BWLBadgeFactory is ERC1155, Ownable {
     ) external onlyOwner {
         _burnBatch(_from, _burnIds, _burnAmounts);
         _mintBatch(_from, _mintIds, _mintAmounts, "");
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) public payable override {
-        require(
-            from == _msgSender() || isApprovedForAll(from, _msgSender()),
-            "ERC1155: caller is not token owner nor approved"
-        );
-        if(isApprovedForAll(from, _msgSender())) {
-
-        } else {
-            
-        }
-    
-        _safeTransferFrom(from, to, id, amount, data);
-    }
-
-    function safeBatchTransferFrom(
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public payable override {
-        require(
-            from == _msgSender() || isApprovedForAll(from, _msgSender()),
-            "ERC1155: caller is not token owner nor approved"
-        );
-        for(uint256 i = 0; i < ids.length; i++) {}
-        _safeBatchTransferFrom(from, to, ids, amounts, data);
     }
 
     // Utils
